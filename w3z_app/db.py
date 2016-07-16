8a0e499e59262fa6f394e019524d1894530c71e5
Divyendu Singh <divyendu.z@gmail.com>
2016-07-17 02:49:05 +0530

    refactor: migrate ads to class Ads (#5)
    * refactor: migrate db.py to class DB
    
    * pep8: clean up
    
    * refactor: move ads to separate class

--- w3z_app/db.py
+++ w3z_app/db.py
@@ -27,18 +27,4 @@ class DB:
             return data[1] if short else data[0]
         else:
             return None
-
-    def get_promos(self):
-        return [
-            {
-                'title': 'DigitalOcean.com',
-                'url': "https://www.digitalocean.com/?refcode=efabde48fb30",
-                'description': "Host your application on SSD servers",
-            },
-
-            {
-                'title': 'Uber.com',
-                'url': "https://www.uber.com/invite/ubercode17",
-                'description': "Transportation in minutes with Uber",
-            },
-        ]
+        
