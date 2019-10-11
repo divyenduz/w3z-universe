4f6ac709a51d6749c3c2a0caf47eb23096cb2f70
Divyendu Singh <divyendu.z@gmail.com>
2019-10-12 00:37:47 +0200

    fix(w3z): deprecate

---
+++ index.js
@@ -0,0 +1,52 @@
+const { Photon } = require('@generated/photon')
+
+const photon = new Photon()
+
+module.exports.handler = async event => {
+  if (event.httpMethod === 'GET') {
+    const converted_url =
+      event.pathParameters && event.pathParameters.converted_url
+
+    if (!converted_url) {
+      return {
+        statusCode: 200,
+        headers: {
+          'content-type': 'text/html',
+        },
+        body:
+          'W3Z is deprecated, the urls shortened in the past will work forever.',
+      }
+    }
+
+    try {
+      const link = await photon.links.findOne({
+        where: {
+          converted_url,
+        },
+      })
+      return {
+        statusCode: 302,
+        headers: {
+          Location: link.url,
+        },
+      }
+    } catch (e) {
+      return {
+        statusCode: 404,
+        headers: {
+          'content-type': 'text/html',
+        },
+        body: 'What are you looking for ? 404!',
+      }
+    }
+  } else {
+    return {
+      statusCode: 200,
+      headers: {
+        'content-type': 'text/html',
+      },
+      body:
+        'W3Z is deprecated, the urls shortened in the past will work forever.',
+    }
+  }
+}
